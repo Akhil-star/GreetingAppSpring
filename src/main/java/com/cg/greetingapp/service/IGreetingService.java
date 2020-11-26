4814843c7b0cebb02a0879c01fe2673ba47442e9
Akhil-star <akhilakki1305@gmail.com>
2020-11-26 08:15:49 +0530

    Spring greeting app

---
+++ src/main/java/com/cg/greetingapp/service/IGreetingService.java
@@ -0,0 +1,18 @@
+package com.cg.greetingapp.service;
+
+import com.cg.greetingapp.model.Greeting;
+import com.cg.greetingapp.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface IGreetingService {
+
+    Greeting addGreeting(User user);
+    Optional<Greeting> getGreetingById(long id);
+    List<Greeting> getAllGreetings();
+    Greeting updateGreeting(long id, String message);
+    void deleteGreeting(long id);
+}
